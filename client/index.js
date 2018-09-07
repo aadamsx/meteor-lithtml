d5a17e8b12b81903089c86c21787044823601ae8
Aaron Adams <aaron@formspoint.com>
2018-09-07 16:54:16 -0500

    updated Meteor version added scripts to index.html

--- client/index.js
+++ client/index.js
@@ -1,4 +1,4 @@
-import { LitElement, html, property } from '@polymer/lit-element';
+import { LitElement, html } from '@polymer/lit-element';
 import './x-counter.js';
 
 // imperative way to interact with web component
