e3e878362dc6f9342d0d797847e5d0fe510d1535
Aaron Adams <aaron@formspoint.com>
2018-09-06 21:43:27 -0500

    added counter and app elements

--- client/index.js
+++ client/index.js
@@ -1,29 +1,21 @@
 import { LitElement, html, property } from '@polymer/lit-element';
+import './x-counter.js';
 
-class XCounter extends LitElement {
-  // Optional, if using TypeScript or Babel experimental decorators are available
-  @property({type: Number})
-  value = 0;
-
-  // If not using Babel or TypeScript you can define properties with vanilla JS
-  // static get properties() {
-  //   return {
-  //     value: 0
-  //   }
-  // }
+class XApp extends LitElement {
+  customValue = 2;
 
   render() {
     return html`
-      <style>
-        button, p {
-          display: inline-block;
-        }
-      </style>
-      <button @click="${() => this.value--}" aria-label="decrement">-</button>
-      <p>${this.value}</p>
-      <button @click="${() => this.value++}" aria-label="increment">+</button>
+      <x-counter
+        @valueChange=${(e) => this.log(e)}
+        .value="${this.customValue}">
+      </x-counter>
     `;
+  }
+
+  log(e) {
+    console.log(e);
   }
 }
 
-customElements.define('x-counter', XCounter);
+customElements.define('x-app', XApp);
---
+++ client/x-counter.js
@@ -0,0 +1,37 @@
+import { LitElement, html, property } from '@polymer/lit-element';
+
+class XCounter extends LitElement {
+  // Decorator
+  @property({type: Number})
+  value = 0;
+
+  render() {
+    return html`
+      <style>
+        button, p {
+          display: inline-block;
+        }
+      </style>
+      <button @click="${() => this.decrement()}" aria-label="decrement">-</button>
+      <p>${this.value}</p>
+      <button @click="${() => this.increment()}" aria-label="increment">+</button>
+    `;
+  }
+
+  decrement() {
+    this.value--;
+    this._valueChanged();
+  }
+
+  increment() {
+    this.value++;
+    this._valueChanged();
+  }
+
+  _valueChanged() {
+    // Fire a custom event for others to listen to
+    this.dispatchEvent(new CustomEvent('valueChange', { detail: this.value }));
+  }
+}
+
+customElements.define('x-counter', XCounter);
